94397dad6c144345adc8ed92095e22284f7f1404
Korenchuk Rustam <smohghtxx@gmail.com>
2024-06-21 14:09:09 +0600

    url ""

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", index)
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def index
-    pass
+
+def index(request):
+    return render(request, "index.html")
