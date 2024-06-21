f951355d197265b15c0a5e0cf2b6de03159b6dd6
Korenchuk Rustam <smohghtxx@gmail.com>
2024-06-21 14:34:39 +0600

    about

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import index
+from webapp.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index)
+    path("", index),
+    path("about/", about)
 ]
